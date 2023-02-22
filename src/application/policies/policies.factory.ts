90e9ec03fc4165d39c020c777860eac9d3551363
Jefferson Gibin <jefferson@jefferson.gibin>
2023-02-22 12:07:05 -0300

    [before release] added comments in the PoliciesFactory

--- src/application/policies/policies.factory.ts
+++ src/application/policies/policies.factory.ts
@@ -1,5 +1,6 @@
 import { IRateLimitCache } from "../../interfaces/cache";
 import { PolicieRateLimit } from "../../interfaces/policies";
+import { RateLimitPolicy } from "./abstract/rate-limit.policy";
 import { RateLimitPerMinutesPolicy } from "./rate-limit-per-minutes.policy";
 import { RateLimitPerPeriodPolicy } from "./rate-limit-per-period.policy";
 import { RateLimitPerSecondsPolicy } from "./rate-limit-per-seconds.policy";
@@ -8,12 +9,24 @@ export class PoliciesFactory {
   private policy: PolicieRateLimit;
   private responseHit: IRateLimitCache;
 
-  constructor(policy: PolicieRateLimit, responseHit: IRateLimitCache) {
+  /**
+   * Simple Factory class to create new instances of  policies classes
+   * @param {PolicieRateLimit} policy object value of settings library
+   * @param {IRateLimitCache} responseRateLimitCache object value of result cache
+   */
+  constructor(
+    policy: PolicieRateLimit,
+    responseRateLimitCache: IRateLimitCache
+  ) {
     this.policy = policy;
-    this.responseHit = responseHit;
+    this.responseHit = responseRateLimitCache;
   }
 
-  create() {
+  /**
+   * Create an instance of super type RateLimitPolicy
+   * @returns {RateLimitPolicy} instance
+   */
+  create(): RateLimitPolicy {
     if (this.policy.type === "REQUEST_PER_SECONDS") {
       return new RateLimitPerSecondsPolicy(this.policy, this.responseHit);
     }
